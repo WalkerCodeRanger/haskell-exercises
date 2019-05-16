b1faf9fedba8a197b10d4672ce276c76c23d0c63
Jeff Walker <Jeff@WalkerCodeRanger.com>
2019-05-16 11:02:10 -0400

    finish homework2 ex 1

--- homework2/src/LogAnalysis.hs
+++ homework2/src/LogAnalysis.hs
@@ -3,6 +3,7 @@ module LogAnalysis where
 import Log
 
 -- Exercise 1
+
 -- Parse a log line
 parseMessage :: String -> LogMessage
 parseMessage line = let parts = (words line) in
@@ -11,3 +12,7 @@ parseMessage line = let parts = (words line) in
     "W":timeStamp:msg -> LogMessage Warning (read timeStamp) (unwords msg)
     "E":errorNum:timeStamp:msg -> LogMessage (Error (read errorNum)) (read timeStamp) (unwords msg)
     _ -> Unknown line
+
+-- Parse a whole log file
+parse :: String -> [LogMessage]
+parse file = map parseMessage (lines file)
