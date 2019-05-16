1798a79d770ff5cd028b869cf9855a3278b7ba2b
Jeff Walker <Jeff@WalkerCodeRanger.com>
2019-05-16 10:56:10 -0400

    first part of homework 2 ex 1

---
+++ homework2/src/LogAnalysis.hs
@@ -0,0 +1,13 @@
+{-# OPTIONS_GHC -Wall #-}
+module LogAnalysis where
+import Log
+
+-- Exercise 1
+-- Parse a log line
+parseMessage :: String -> LogMessage
+parseMessage line = let parts = (words line) in
+  case parts of
+    "I":timeStamp:msg -> LogMessage Info (read timeStamp) (unwords msg)
+    "W":timeStamp:msg -> LogMessage Warning (read timeStamp) (unwords msg)
+    "E":errorNum:timeStamp:msg -> LogMessage (Error (read errorNum)) (read timeStamp) (unwords msg)
+    _ -> Unknown line
--- homework2/test/Spec.hs
+++ homework2/test/Spec.hs
@@ -1,2 +1,17 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+import Test.HUnit
+import Log
+import LogAnalysis
+import Lib
+
+parseMessageCases = [
+    ("E 2 562 help help", LogMessage (Error 2) 562 "help help"),
+    ("I 29 la la la", LogMessage Info 29 "la la la"),
+    ("W 42 A warning", LogMessage Warning 42 "A warning"),
+    ("This is not in the right format", Unknown "This is not in the right format")
+  ]
+
+testParseMessageCase line expected = TestCase $ assertEqual ("for (parseMessage "++(show line)++"),") expected (parseMessage line)
+testParseMessage = TestLabel "parseMessage" $ TestList (map (uncurry testParseMessageCase) parseMessageCases)
+
+main :: IO Counts
+main = runTestTT $ TestList [testParseMessage]
