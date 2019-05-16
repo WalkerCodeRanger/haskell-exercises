6c294ea5b0ec86792cf9e1d7d04d65ecce914d9f
Jeff Walker <Jeff@WalkerCodeRanger.com>
2019-05-16 14:21:00 -0400

    further shorten homework 3 ex 1

--- homework3/src/Golf.hs
+++ homework3/src/Golf.hs
@@ -6,11 +6,11 @@ module Golf where
 -- second is every other item in the list, the third is every third item etc.
 -- The result will be a number of lists equal to the length of the original list
 skips :: [a] -> [[a]]
-skips xs = takeWhile (not . null) $ map (skip xs) [0..]
+skips l = skip l <$> [0..length l - 1]
 
 -- Skips items from a list, taking the items in between. i.e. skip 1 will give
 -- every other item.
 skip :: [a] -> Int -> [a]
 skip l n = case drop n l of
   [] -> []
-  x:xs -> x:(skip xs n)
+  x:r -> x:(skip r n)
