51a08e85d02d64345e8905ee3238464a93aba040
Jeff Walker <Jeff@WalkerCodeRanger.com>
2019-05-16 21:39:45 -0400

    homework 3 ex 3

--- homework3/src/Golf.hs
+++ homework3/src/Golf.hs
@@ -1,7 +1,11 @@
 module Golf where
 
+import Data.List
+
 -- Note: The stated goal of this is to get the actual code as short as possible,
 -- not counting imports, comments, whitespace and type declarations
+--
+-- **Because of this, the names are shorter than I would like**
 
 -- Exercise 1
 
@@ -26,3 +30,29 @@ localMaxima :: [Integer] -> [Integer]
 localMaxima (a:b:c:r) = let l = localMaxima $ b:c:r in
   if a < b && b > c then b:l else l
 localMaxima _ = []
+
+
+-- Exercise 3
+
+-- Generate a histogram of the integers in a list. We are allowed to assume they
+-- are all in the range 0..9 (values outside the range are ignored)
+histogram :: [Integer] -> String
+histogram v = unlines . transpose . padBars $ (bar v) <$> [0..9]
+
+-- Generate a string that represents the bar for an individual value. Note that
+-- due to the way transpose works, the bar starts with the asterisks and ends
+-- with the value (i.e. "****=2").
+bar :: [Integer] -> Integer -> [Char]
+bar v n = replicate (count n v) '*' ++ "=" ++ show n
+
+-- Pad out the bar strings so they are all the same length
+padBars :: (Functor f, Foldable f) => f [Char] -> f [Char]
+padBars b = pad (maximum (length <$> b)) <$> b
+
+-- Count the number of occurrences of a value in a list
+count :: Integer -> [Integer] -> Int
+count n = length . filter (==n)
+
+-- Left pads a string with spaces out to a given length
+pad :: Int -> [Char] -> [Char]
+pad n s = replicate (n - length s) ' ' ++ s
--- homework3/test/Spec.hs
+++ homework3/test/Spec.hs
@@ -1,7 +1,6 @@
 import Test.HUnit
 import Golf
 
-skipsCases :: [(String, [String])]
 skipsCases = [
     ("ABCD", ["ABCD", "BD", "C", "D"]),
     ("hello!", ["hello!", "el!", "l!", "l", "o", "!"])--,
@@ -10,23 +9,18 @@ skipsCases = [
     --([], [])
   ]
 
-testSkipsCase :: (Eq a, Show a) => [a] -> [[a]] -> Test
 testSkipsCase list expected = TestCase $ assertEqual ("for (skips "++(show list)++"),") expected (skips list)
 
-testSkips :: Test
 testSkips = TestLabel "skips" $ TestList (map (uncurry testSkipsCase) skipsCases)
 
-localMaximaCases :: [([Integer], [Integer])]
 localMaximaCases = [
     ([2,9,5,6,1], [9,6]),
     ([2,3,4,1,5], [4]),
     ([1,2,3,4,5], [])
   ]
 
-testLocalMaximaCase :: [Integer] -> [Integer] -> Test
 testLocalMaximaCase list expected = TestCase $ assertEqual ("for (localMaxima "++(show list)++"),") expected (localMaxima list)
 
-testLocalMaxima :: Test
 testLocalMaxima = TestLabel "localMaxima" $ TestList (map (uncurry testLocalMaximaCase) localMaximaCases)
 
 main :: IO Counts
