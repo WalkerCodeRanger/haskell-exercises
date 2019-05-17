a20ac9bf4ed54e0d997121d068f96960ebfc5449
Jeff Walker <Jeff@WalkerCodeRanger.com>
2019-05-17 10:02:05 -0400

    add quick check to the project (not using yet)

--- homework4/test/Spec.hs
+++ homework4/test/Spec.hs
@@ -1,11 +1,28 @@
 import Test.Tasty
 -- import Test.Tasty.SmallCheck as SC
--- import Test.Tasty.QuickCheck as QC
-import Test.Tasty.HUnit
+import Test.Tasty.QuickCheck as QC
+-- import Test.Tasty.HUnit
+
+import Data.List
+import Lib
 
 main = defaultMain tests
 
 tests :: TestTree
-tests = testGroup "Tests" [{--properties, unitTests--}]
+tests = testGroup "Tests" [qcProps]
 
 
+qcProps = testGroup "(checked by QuickCheck)"
+  [{-- QC.testProperty "fun1 == fun1'" $
+      \list -> fun1 list == fun1' list
+  , QC.testProperty "fun2 == fun2'" $
+      \x -> fun2 x == fun2' x
+  ,--} QC.testProperty "sort == sort . reverse" $
+      \list -> sort (list :: [Integer]) == sort (reverse list)
+  , QC.testProperty "Fermat's little theorem" $
+      \x -> ((x :: Integer)^7 - x) `mod` 7 == 0
+  -- the following property does not hold
+  , QC.testProperty "Fermat's last theorem" $
+      \x y z n ->
+        (n :: Integer) >= 3 QC.==> x^n + y^n /= (z^n :: Integer)
+  ]
