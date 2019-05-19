d89416c186ab348662db654e0a564b31eaae9f10
Jeff Walker <Jeff@WalkerCodeRanger.com>
2019-05-19 13:06:18 -0400

    homework 5 ex 4

--- homework5/src/Calc.hs
+++ homework5/src/Calc.hs
@@ -1,5 +1,6 @@
 module Calc where
 
+import Data.Coerce
 import ExprT
 import Parser
 
@@ -14,6 +15,7 @@ evalStr :: String -> Maybe Integer
 evalStr s = eval <$> parseExp Lit Add Mul s
 
 -- Exercise 3
+
 class Expr a where
   lit :: Integer -> a
   add :: a -> a -> a
@@ -23,3 +25,29 @@ instance Expr ExprT where
   lit = Lit
   add = Add
   mul = Mul
+
+-- Exercise 4
+
+instance Expr Integer where
+  lit i = i
+  add x y = x + y
+  mul x y = x * y
+
+instance Expr Bool where
+  lit b = b > 0
+  add x y = x || y
+  mul x y = x && y
+
+-- These new types provided with question:
+newtype MinMax = MinMax Integer deriving (Eq, Show)
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+
+instance Expr MinMax where
+  lit i = MinMax i
+  add x y = MinMax $ max (coerce x) (coerce y)
+  mul x y = MinMax $ min (coerce x) (coerce y)
+
+instance Expr Mod7 where
+  lit i = Mod7 $ i `mod` 7
+  add x y = Mod7 $ ((coerce x) + (coerce y)) `mod` 7
+  mul x y = Mod7 $ ((coerce x) * (coerce y)) `mod` 7
--- homework5/test/Spec.hs
+++ homework5/test/Spec.hs
@@ -2,12 +2,21 @@ import Test.Tasty
 import Test.Tasty.QuickCheck as QC
 import Test.Tasty.HUnit
 
+import Parser
 import ExprT
 import Calc
 
 main = defaultMain tests
 
 tests = testGroup "Tests" [unitTests]
+
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul "(3 * -4) + 5"
+
+testInteger = testExp :: Maybe Integer
+testBool = testExp :: Maybe Bool
+testMM = testExp :: Maybe MinMax
+testM7 = testExp :: Maybe Mod7
 
 unitTests = testGroup "Unit tests"
   [ testCase "eval (Mul (Add (Lit 2) (Lit 3)) (Lit 4)) == 20" $
@@ -21,4 +30,13 @@ unitTests = testGroup "Unit tests"
     Nothing @=? evalStr "2+3*"
   , testCase "mul (add (lit 2) (lit 3)) (lit 4) :: ExprT == Mul (Add (Lit 2) (Lit 3)) (Lit 4)" $
     Mul (Add (Lit 2) (Lit 3)) (Lit 4) @=? ((mul (add (lit 2) (lit 3)) (lit 4)) :: ExprT)
+
+  , testCase "testInteger" $
+    Just (-7) @=? testInteger
+  , testCase "testBool" $
+    Just True @=? testBool
+  , testCase "testMM" $
+    Just (MinMax 5) @=? testMM
+  , testCase "testM7" $
+    Just (Mod7 0) @=? testM7
   ]
