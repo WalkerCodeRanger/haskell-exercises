4aafc794250513680743360be8d7ebc636f8ee3c
Jeff Walker <Jeff@WalkerCodeRanger.com>
2019-05-24 13:47:16 -0400

    setup project for homework 6

---
+++ homework6/README.md
@@ -0,0 +1,3 @@
+# Homework 6
+
+Solutions to UPenn CIS 149 (Spring 2013) [Homework 6](https://www.seas.upenn.edu/~cis194/spring13/hw/06-laziness.pdf).
---
+++ homework6/test/Spec.hs
@@ -0,0 +1,7 @@
+import Test.Tasty
+import Test.Tasty.QuickCheck as QC
+import Test.Tasty.HUnit
+
+main = defaultMain tests
+
+tests = testGroup "Tests" []
