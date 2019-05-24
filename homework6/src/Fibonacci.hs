ee61816599ad6f5705421d06bebf9e685ea3c1b3
Jeff Walker <Jeff@WalkerCodeRanger.com>
2019-05-24 15:20:41 -0400

    homework 6 exercise 6 & 7

--- homework6/src/Fibonacci.hs
+++ homework6/src/Fibonacci.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# OPTIONS_GHC -fno-warn-missing-methods #-}
+
 module Fibonacci where
 
 -- Exercise 1
@@ -61,3 +64,44 @@ interleaveStreams (Cons x xs) ys = Cons x (interleaveStreams ys xs)
 -- Eric D.Burgess - http://oeis.org/A001511
 startRuler :: Integer -> Stream Integer
 startRuler x = interleaveStreams (streamRepeat x) (startRuler (x+1))
+
+-- Exercise 6
+
+-- This gives a bunch of warnings, but I'm not going to go back and rename
+-- everything just because the homework has a stupid unusual variable name
+x :: Stream Integer
+x = Cons 0 (Cons 1 (streamRepeat 0))
+
+instance Num (Stream Integer) where
+  fromInteger n = Cons n (streamRepeat 0)
+  negate = streamMap negate
+  (+) (Cons a0 a') (Cons b0 b') = Cons (a0 + b0) (a' + b')
+  (*) (Cons a0 a') b@(Cons b0 b') = Cons (a0 * b0) (streamMap (*a0) b' + a' * b)
+
+instance Fractional (Stream Integer) where
+  (/) a@(Cons a0 a') b@(Cons b0 b') = Cons (a0 `div` b0) (streamMap (`div` b0) (a' - a/b*b'))
+
+fibs3 :: Stream Integer
+fibs3 = x / (1 - x - x^2)
+
+-- Exercise 7
+data Matrix = Matrix Integer Integer
+                     Integer Integer
+
+instance Num Matrix where
+  (*) (Matrix a11 a12
+              a21 a22)
+      (Matrix b11 b12
+              b21 b22)
+    = Matrix (a11*b11+a12*b21) (a11*b12+a12*b22)
+             (a21*b11+a22*b21) (a21*b12+a22*b22)
+
+fibMatrix :: Matrix
+fibMatrix = Matrix 1 1
+                   1 0
+
+fib4 :: Integer -> Integer
+fib4 0 = 0
+fib4 n = fibOf (fibMatrix^n)
+  where fibOf (Matrix _ x _ _) = x
+
