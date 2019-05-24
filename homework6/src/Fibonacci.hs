25fede178fd3679965da1380265cba0e88ce5bb7
Jeff Walker <Jeff@WalkerCodeRanger.com>
2019-05-24 14:44:47 -0400

    homework 6 ex 4 and 5

--- homework6/src/Fibonacci.hs
+++ homework6/src/Fibonacci.hs
@@ -25,3 +25,39 @@ streamToList (Cons x xs) = x : streamToList xs
 
 instance Show a => Show (Stream a) where
   show s = show $ take 20 $ streamToList s
+
+-- Exercise 4
+
+streamRepeat :: a -> Stream a
+streamRepeat x = Cons x (streamRepeat x)
+
+streamMap :: (a -> b) -> Stream a -> Stream b
+streamMap f (Cons x xs) = Cons (f x) (streamMap f xs)
+
+streamFromSeed :: (a -> a) -> a -> Stream a
+streamFromSeed f s = Cons s (streamFromSeed f (f s))
+
+-- Exercise 5
+
+nats :: Stream Integer
+nats = streamFromSeed (+1) 0
+
+-- the "ruler function" 0, 1, 0, 2, 0, 1, 0, 3, 0, 1, 0, 2, 0, 1, 0, 4, . . .
+-- where the nth element in the stream (assuming the first element corresponds
+-- to n = 1) is the largest power of 2 which evenly divides n.
+ruler :: Stream Integer
+ruler = startRuler 0
+
+interleaveStreams :: Stream a -> Stream a -> Stream a
+-- This is the version I wrote first which was causing stack overflow because it
+-- forced the evaluation of the second argument which was causing infinitive
+-- recursion on `startRuler x+1`.
+--interleaveStreams (Cons x xs) (Cons y ys) = Cons x (Cons y (interleaveStreams xs ys))
+-- I found this version in another persons solution. It makes sense when I read
+-- it, but the other form seemed much more natural to me.
+interleaveStreams (Cons x xs) ys = Cons x (interleaveStreams ys xs)
+
+-- Even with the hint, had to look up how to form this sequence using interleaving
+-- Eric D.Burgess - http://oeis.org/A001511
+startRuler :: Integer -> Stream Integer
+startRuler x = interleaveStreams (streamRepeat x) (startRuler (x+1))
--- homework6/test/Spec.hs
+++ homework6/test/Spec.hs
@@ -15,4 +15,14 @@ unitTests = testGroup "Unit tests"
   , testCase "take 15 fibs2" $
     [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377]
       @=? take 15 fibs1
+  , testCase "streamRepeat 5" $
+    take 100 (repeat 5) @=? take 100 (streamToList $ streamRepeat 5)
+  , testCase "streamMap" $
+    take 100 (repeat 10) @=? take 100 (streamToList $ streamMap (*2) (streamRepeat 5))
+  , testCase "streamFromSeed" $
+    take 100 (iterate (*2) 1) @=? take 100 (streamToList $ streamFromSeed (*2) 1)
+  , testCase "nats" $
+    take 100 [0..] @=? take 100 (streamToList nats)
+  , testCase "ruler" $
+    [0, 1, 0, 2, 0, 1, 0, 3, 0, 1, 0, 2, 0, 1, 0, 4] @=? take 16 (streamToList ruler)
   ]
