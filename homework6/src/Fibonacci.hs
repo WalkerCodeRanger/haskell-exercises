86164ea10f6d9a1d17339abba92f4a48f924b20a
Jeff Walker <Jeff@WalkerCodeRanger.com>
2019-05-24 13:57:16 -0400

    homework 6 exercise 2

--- homework6/src/Fibonacci.hs
+++ homework6/src/Fibonacci.hs
@@ -11,3 +11,8 @@ fib n = fib (n - 1) + fib (n - 2)
 -- Define the Fibonacci sequence in terms of the `fib` function (yes, this will be slow)
 fibs1 :: [Integer]
 fibs1 = fib <$> [0..]
+
+-- Exercise 2
+fibs2 :: [Integer]
+fibs2 = 0 : 1 : fibs 0 1
+  where fibs x y = let z = x + y in z : fibs y z
--- homework6/test/Spec.hs
+++ homework6/test/Spec.hs
@@ -12,4 +12,7 @@ unitTests = testGroup "Unit tests"
   [ testCase "take 15 fibs1" $
     [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377]
       @=? take 15 fibs1
+  , testCase "take 15 fibs2" $
+    [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377]
+      @=? take 15 fibs1
   ]
