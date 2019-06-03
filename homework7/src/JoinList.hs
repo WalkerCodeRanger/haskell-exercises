42b9348877b0a19d78e1917439e7070d66f5f136
Jeff Walker <Jeff@WalkerCodeRanger.com>
2019-06-03 12:41:48 -0400

    homework 7 ex 2

--- homework7/src/JoinList.hs
+++ homework7/src/JoinList.hs
@@ -1,5 +1,7 @@
 module JoinList where
 
+import Sized
+
 -- The JoinList type was provided with the homework
 data JoinList m a = Empty
   | Single m a
@@ -14,3 +16,18 @@ tag :: Monoid m => JoinList m a -> m
 tag Empty = mempty
 tag (Single m _) = m
 tag (Append m _ _) = m
+
+-- Exercise 2
+
+-- For convenience, I make any join list with a Sized, Monoid annotation Sized
+instance (Sized m, Monoid m) => Sized (JoinList m a) where
+  size = size . tag
+
+-- Find the join list element at the specified index. It should be the case that
+-- (indexJ i jl) == (jlToList jl !!? i) for any index `i` and join-list `jl`
+indexJ :: (Sized m, Monoid m) => Int -> JoinList m a -> Maybe a
+indexJ i jl | i < 0 || i >= getSize (size jl) = Nothing
+indexJ 0 (Single _ a) = Just a
+indexJ i (Append _ l r) = let leftSize = getSize (size l) in
+   if i < leftSize then indexJ i l else indexJ (i-leftSize) r
+
--- homework7/test/Spec.hs
+++ homework7/test/Spec.hs
@@ -2,6 +2,37 @@ import Test.Tasty
 import Test.Tasty.QuickCheck as QC
 import Test.Tasty.HUnit
 
+import JoinList
+import Sized
+
 main = defaultMain tests
 
-tests = testGroup "Tests" []
+tests = testGroup "Tests" [unitTests]
+
+-- Functions to help build a sample join list
+val :: a -> JoinList Size a
+val v = Single 1 v
+
+sampleJL :: JoinList Size Char
+sampleJL = (val 'y' +++ (val 'e' +++ val 'a')) +++ val 'h'
+
+-- Safe list indexing function provided with the homework
+(!!?) :: [a] -> Int -> Maybe a
+[] !!? _ = Nothing
+_ !!? i | i < 0 = Nothing
+(x:xs) !!? 0 = Just x
+(x:xs) !!? i = xs !!? (i-1)
+
+-- Join list to list function provided with the question
+jlToList :: JoinList m a -> [a]
+jlToList Empty = []
+jlToList (Single _ a) = [a]
+jlToList (Append _ l1 l2) = jlToList l1 ++ jlToList l2
+
+unitTests = testGroup "Unit tests"
+  [ testCase "+++" $
+    ['y', 'e', 'a', 'h'] @=? jlToList sampleJL
+  , testCase "indexJ" $
+    (all (==True) [(jlToList sampleJL !!? i) == (indexJ i sampleJL) | i <- [-1..5]])
+      @? "not equivalent to safe list indexing"
+  ]
