a8b72356ab7d05da6dccf73daa78d22161664999
Jeff Walker <Jeff@WalkerCodeRanger.com>
2019-06-03 11:51:06 -0400

    get provided editor code running

--- homework7/app/Main.hs
+++ homework7/app/Main.hs
@@ -1,4 +1,14 @@
 module Main where
 
+import StringBuffer
+import Editor
+
+-- This version of main was provided with the homework in the
+-- "StringBufEditor.hs" file.
 main :: IO ()
-main = putStrLn "Not yet implemented"
+main = runEditor editor $ unlines
+          [ "This buffer is for notes you don't want to save, and for"
+          , "evaluation of steam valve coefficients."
+          , "To load a different file, type the character L followed"
+          , "by the name of the file."
+          ]
